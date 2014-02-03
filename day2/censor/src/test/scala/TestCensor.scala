7ae33ecee89609c401edbd7641f2198b9dd2bed4
Stephen Best <bestie@gmail.com>
2014-02-03 14:03:50 +0000

    Added test and implementation for uncensored string

--- day2/censor/src/test/scala/TestCensor.scala
+++ day2/censor/src/test/scala/TestCensor.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 
 trait Censor {
   def censorString(string: String) : String = {
-    ""
+    string
   }
 }
 
@@ -11,6 +11,12 @@ class TestCensor extends FunSpec with ShouldMatchers with Censor {
     describe("when given an empty string") {
       it("should return an empty string") {
         censorString("") should be ("")
+      }
+    }
+
+    describe("when the string contains no censored words") {
+      it("should return the original string") {
+        censorString("Bite me!") should be ("Bite me!")
       }
     }
   }
