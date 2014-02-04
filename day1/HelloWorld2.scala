1cf186f7586e70bfb382651febef328afa6e0a6f
Sleepy Fox <sleepyfox@gmail.com>
2014-02-04 11:45:22 +0000

    added day1 code examples from text

---
+++ day1/HelloWorld2.scala
@@ -0,0 +1,5 @@
+object HelloWorld2 extends App {
+  println("Hello App!")
+}
+
+
---
+++ day1/ruby_for_loop.scala
@@ -0,0 +1,7 @@
+def rubyStleForLoop {
+	println("for loop using Ruby-style iteration")
+	args.foreach { arg =>
+		println(arg)
+	}
+}
+rubyStleForLoop
